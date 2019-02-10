d7965e39248fae7d0e1dff48eb0c74fc05bb41c0
kalvo19 <recmanvideos@gmail.com>
2019-02-10 19:46:48 +0100

    Starting the site

---
+++ history/main.js
@@ -0,0 +1,9 @@
+
+
+// 1-- Introducing history object
+window.console.log(history);
+
+// 2-- Showing all the history of the current window or tab (it works for both)
+console.log(history.length);
+
+// 3--
---
+++ window/main.js
@@ -0,0 +1,20 @@
+
+
+// 1-- Using javascript console explicitly
+window.console.log("A que no sabeis quién soy: ");
+
+// 2-- Using JavaScript console implicitly
+console.log("Soy el Stifmeister");
+
+// 3-- Calling the document object explicitly
+window.console.log(window.document);
+
+// 4-- Calling the document object implicitly
+console.log(document);
+
+//
+
+// 6-- Showing all the history of the current window or tab (it works for both)
+console.log(history.length);
+
+// 6--
